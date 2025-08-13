4279faff223b503b6175902ccc8e8c9fd16002cd
gavindu-gayashan <gavindukariyawasam@gmail.com>
2025-08-13 16:58:46 +0530

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -8,11 +8,11 @@ dotenv.config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "bms",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQLHOST || process.env.DB_HOST || "localhost",
+  user: process.env.MYSQLUSER || process.env.DB_USER || "root",
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || "root",
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || "bms",
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
   timezone: '+05:30', // Set to Sri Lanka timezone
   dateStrings: [
     'DATE',
